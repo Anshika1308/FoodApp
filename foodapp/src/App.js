4f5f2fb055f2ea2306a49a98285d12a67df879c8
ganshika <ganshika@bridgetree.com>
2022-01-05 13:50:42 +0530

    FoodApp

---
+++ foodapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import Tabs from "./component/TabMenu/Tabs";
+import Gallery from "./component/TabMenu/Gallery";
+
+const App = () => {
+  return (
+    <>
+     <Gallery/>
+    </>
+  )
+}
+
+export default App
